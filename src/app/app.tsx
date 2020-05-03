9bb4bac7d348d322b2e674028eb0d8bad94f9c21
Chris Camacho <cacamacho91@gmail.com>
2020-05-03 15:53:04 +0100

    v1 setup with no createreact-app

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import {Home} from "../containers"
+
+export default function App() {
+  return <Home />
+}
---
+++ src/containers/Home.tsx
@@ -0,0 +1,9 @@
+import React, {PureComponent} from "react"
+
+class Home extends PureComponent {
+  render() {
+    return <p>I am home</p>
+  }
+}
+
+export default Home
